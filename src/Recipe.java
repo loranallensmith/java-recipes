cb7279411b3788ceade707cc4b1c7930bae9b215
Allen Smith <loranallensmith@github.com>
2017-09-06 20:05:32 -0400

    Update main method

--- src/Recipe.java
+++ src/Recipe.java
@@ -2,7 +2,7 @@
 public class Recipe {
 
 	public static void main(String[] args) {
-		System.out.println("Hello world!");
+		System.out.println("Hello world! This is my Recipes Application");
 	}
 
 }
